6a72457f1a934014f0c7f2bdc67c27addf23dea2
jalpate151 <jalpate151@SDS-L-BB1HC63>
2024-10-24 11:25:13 -0400

    Add project files.

---
+++ ass/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace ass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.option3Button = new System.Windows.Forms.Button();
+            this.option2Button = new System.Windows.Forms.Button();
+            this.option1Button = new System.Windows.Forms.Button();
+            this.outputBox = new System.Windows.Forms.Label();
+            this.option3 = new System.Windows.Forms.Label();
+            this.option2 = new System.Windows.Forms.Label();
+            this.option1 = new System.Windows.Forms.Label();
+            this.imageBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // option3Button
+            // 
+            this.option3Button.Location = new System.Drawing.Point(62, 498);
+            this.option3Button.Name = "option3Button";
+            this.option3Button.Size = new System.Drawing.Size(75, 23);
+            this.option3Button.TabIndex = 16;
+            this.option3Button.Text = "button3";
+            this.option3Button.UseVisualStyleBackColor = true;
+            // 
+            // option2Button
+            // 
+            this.option2Button.Location = new System.Drawing.Point(62, 435);
+            this.option2Button.Name = "option2Button";
+            this.option2Button.Size = new System.Drawing.Size(75, 23);
+            this.option2Button.TabIndex = 15;
+            this.option2Button.Text = "button2";
+            this.option2Button.UseVisualStyleBackColor = true;
+            this.option2Button.Click += new System.EventHandler(this.option2Button_Click);
+            // 
+            // option1Button
+            // 
+            this.option1Button.Location = new System.Drawing.Point(62, 388);
+            this.option1Button.Name = "option1Button";
+            this.option1Button.Size = new System.Drawing.Size(75, 23);
+            this.option1Button.TabIndex = 14;
+            this.option1Button.Text = "button1";
+            this.option1Button.UseVisualStyleBackColor = true;
+            this.option1Button.Click += new System.EventHandler(this.option1Button_Click);
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(31, 9);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(832, 82);
+            this.outputBox.TabIndex = 13;
+            // 
+            // option3
+            // 
+            this.option3.AutoSize = true;
+            this.option3.Location = new System.Drawing.Point(162, 503);
+            this.option3.Name = "option3";
+            this.option3.Size = new System.Drawing.Size(35, 13);
+            this.option3.TabIndex = 12;
+            this.option3.Text = "label3";
+            // 
+            // option2
+            // 
+            this.option2.AutoSize = true;
+            this.option2.Location = new System.Drawing.Point(162, 440);
+            this.option2.Name = "option2";
+            this.option2.Size = new System.Drawing.Size(35, 13);
+            this.option2.TabIndex = 11;
+            this.option2.Text = "label2";
+            // 
+            // option1
+            // 
+            this.option1.AutoSize = true;
+            this.option1.Location = new System.Drawing.Point(162, 393);
+            this.option1.Name = "option1";
+            this.option1.Size = new System.Drawing.Size(35, 13);
+            this.option1.TabIndex = 10;
+            this.option1.Text = "label1";
+            // 
+            // imageBox
+            // 
+            this.imageBox.Location = new System.Drawing.Point(182, 107);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(515, 270);
+            this.imageBox.TabIndex = 9;
+            this.imageBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(941, 531);
+            this.Controls.Add(this.option3Button);
+            this.Controls.Add(this.option2Button);
+            this.Controls.Add(this.option1Button);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.option3);
+            this.Controls.Add(this.option2);
+            this.Controls.Add(this.option1);
+            this.Controls.Add(this.imageBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button option3Button;
+        private System.Windows.Forms.Button option2Button;
+        private System.Windows.Forms.Button option1Button;
+        private System.Windows.Forms.Label outputBox;
+        private System.Windows.Forms.Label option3;
+        private System.Windows.Forms.Label option2;
+        private System.Windows.Forms.Label option1;
+        private System.Windows.Forms.PictureBox imageBox;
+    }
+}
+
---
+++ ass/Form1.cs
@@ -0,0 +1,414 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ass
+{
+    public partial class Form1 : Form
+    {
+        int page;
+        public Form1()
+        {
+            InitializeComponent();
+            outputBox.Text = "you are walking in a dark alley and discover a haunted house do you enter through the front door or back door";
+            option1.Text = "front door ";
+            option2.Text = "back door";
+        }
+
+        private void option1Button_Click(object sender, EventArgs e)
+        {
+            {
+                if (page == 1)
+                {
+                    page = 2;
+                }
+                else if (page == 2)
+                {
+                    page = 5;
+                }
+                if (page == 3)
+                {
+                    page = 99;
+                }
+                else if (page == 4)
+                {
+                    page = 00;
+                }
+                if (page == 5)
+                {
+                    page = 6;
+                }
+                else if (page == 6)
+                {
+
+                    Random randGen = new Random();
+                    int random = randGen.Next(0, 100);
+                    if (random > 50)
+                    {
+                        page = 10;
+                    }
+                    else
+                    {
+                        page = 11;
+                    }
+
+                }
+                if (page == 7)
+                {
+                    page = 99;
+                }
+
+
+
+                if (page == 9)
+                {
+                    page = 99;
+                }
+                else if (page == 10)
+                {
+                    page = 99;
+                }
+                if (page == 11)
+                {
+                    page = 99;
+                }
+                else if (page == 12)
+                {
+                    page = 13;
+                }
+                if (page == 13)
+                {
+                    page = 14;
+                }
+                else if (page == 14)
+                {
+                    page = 16;
+                }
+                if (page == 15)
+                {
+                    page = 99;
+                }
+                else if (page == 16)
+                {
+                    page = 99;
+                }
+                if (page == 17)
+                {
+                    page = 99;
+                }
+                else if (page == 18)
+                {
+                    page = 99;
+                }
+                if (page == 19)
+                {
+                    page = 20;
+                }
+                else if (page == 20)
+                {
+                    page = 21;
+                }
+                if (page == 21)
+                {
+                    page = 99;
+                }
+                else if (page == 22)
+                {
+                    page = 99;
+                }
+                if (page == 23)
+                {
+                    page = 99;
+                }
+                DisplayPage();
+
+            }
+        }
+
+        private void option2Button_Click(object sender, EventArgs e)
+        {
+            {
+                {
+                    if (page == 1)
+                    {
+                        page = 12;
+                    }
+                    else if (page == 19)
+                    {
+                        page = 20;
+                    }
+                    if (page == 23)
+                    {
+                        page = 99;
+                    }
+                    else if (page == 20)
+                    {
+                        page = 22;
+                    }
+                    if (page == 22)
+                    {
+                        page = 99;
+                    }
+                    else if (page == 6)
+                    {
+
+                        Random randGen = new Random();
+                        int random = randGen.Next(0, 100);
+                        if (random > 50)
+                        {
+                            page = 10;
+                        }
+                        else
+                        {
+                            page = 11;
+                        }
+
+                    }
+                    if (page == 13)
+                    {
+                        page = 15;
+                    }
+
+
+
+                    if (page == 15)
+                    {
+                        page = 99;
+                    }
+                    else if (page == 14)
+                    {
+                        page = 17;
+                    }
+                    if (page == 17)
+                    {
+                        page = 99;
+                    }
+                    else if (page == 2)
+                    {
+                        page = 4;
+                    }
+                    if (page == 4)
+                    {
+                        page = 99;
+                    }
+                    else if (page == 5)
+                    {
+                        page = 7;
+                    }
+                    if (page == 6)
+                    {
+                        page = 9;
+                    }
+                    else if (page == 9)
+                    {
+                        page = 99;
+                    }
+                    if (page == 17)
+                    {
+                        page = 99;
+                    }
+                    else if (page == 18)
+                    {
+                        page = 99;
+                    }
+                    if (page == 19)
+                    {
+                        page = 20;
+                    }
+                    else if (page == 20)
+                    {
+                        page = 21;
+                    }
+                    if (page == 21)
+                    {
+                        page = 99;
+                    }
+                    else if (page == 22)
+                    {
+                        page = 99;
+                    }
+                    if (page == 23)
+                    {
+                        page = 99;
+                    }
+                }
+
+            }
+        }
+
+
+        public void DisplayPage()
+        {
+            switch (page)
+            {
+                case 1:
+                    outputBox.Text = "you find a haunted house";
+                    option1.Text = "front door";
+                    option2.Text = "back door";
+                    option3.Text = "";
+
+                    break;
+                case 2:
+                    outputBox.Text = "as you enter the front door opens on its own ";
+                    option1.Text = "go upstairs";
+                    option2.Text = "living room";
+                    option3.Text = "leave";
+                    break;
+                case 3:
+                    outputBox.Text = "as you are leaving you slip on the pavemnet and die ";
+                    option1.Text = "";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+                case 4:
+                    outputBox.Text = "as you step in the living room you get paralzyed and die standing up";
+                    option1.Text = "";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+                case 5:
+                    outputBox.Text = "as you go up the stairs you hear a scream coming from the bedroom";
+                    option1.Text = "bathroom";
+                    option2.Text = "bedroom";
+                    option3.Text = "";
+                    break;
+                case 6:
+                    outputBox.Text = "writien on the wall with blood it say say bloody mary 3 times in the mirro";
+                    option1.Text = "say it 3 times";
+                    option2.Text = "leave the bathroom";
+                    option3.Text = "";
+                    break;
+                case 7:
+                    outputBox.Text = "as you go in the girl crying jumps on you and rips you apart leading to your death";
+                    option1.Text = "";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+                case 8:
+                    outputBox.Text = "";
+                    option1.Text = "";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+                case 9:
+                    outputBox.Text = "as you try to leave she pops in front of you and snaps your neck";
+                    option1.Text = "";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+                case 10:
+                    outputBox.Text = "you say bloddy mary 3 times and she appres in front of the mirror you talk to her but she gets bored and kills you";
+                    option1.Text = "";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+                case 11:
+                    outputBox.Text = "you say bloody mary 3 times and she appres in fron of the mirror you talk to her and she likes you and helps you escape you win";
+                    option1.Text = "";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+                case 12:
+                    outputBox.Text = "do you go to the kitchen or basment";
+                    option1.Text = "kitehcn";
+                    option2.Text = "basment";
+                    option3.Text = "leave";
+                    break;
+                case 13:
+                    outputBox.Text = "as you enter the kithen a rat jumpcares you do you grab a kife or not";
+                    option1.Text = "grab it";
+                    option2.Text = "dont grab it";
+                    option3.Text = "";
+                    break;
+                case 14:
+                    outputBox.Text = "a ghost appres and trys to attack you but you kill it with the kife. do you run out the house or stay";
+                    option1.Text = "run";
+                    option2.Text = "stay";
+                    option3.Text = "";
+                    break;
+                case 15:
+                    outputBox.Text = "a ghost appres and kills you because you didnt have a wepon";
+                    option1.Text = "";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+                case 16:
+                    outputBox.Text = "you run out the house and stay alive";
+                    option1.Text = "";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+                case 17:
+                    outputBox.Text = "another ghost appres and takes your kife and stabs you";
+                    option1.Text = "";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+                case 18:
+                    outputBox.Text = "you leave but as you are leaving a ghost comes and kills you";
+                    option1.Text = "";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+                case 19:
+                    outputBox.Text = "you are going down the staris but slip and fall down";
+                    option1.Text = "";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+                case 20:
+                    outputBox.Text = "";
+                    option1.Text = "";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+                case 21:
+                    outputBox.Text = "";
+                    option1.Text = "";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+                case 22:
+                    outputBox.Text = "";
+                    option1.Text = "";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+                case 23:
+                    outputBox.Text = "";
+                    option1.Text = "";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+                case 99:
+                    outputBox.Text = "";
+                    option1.Text = "";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+
+
+
+
+
+
+
+            }
+        }
+
+
+
+
+    }
+}
+    
+    
+    
+    
+
